9eecd25f5aff5746f26aa610de49aff971f848e3
Chloe Hwang <c.chloeh@gmail.com>
2017-05-23 15:40:50 -0400

    finished scrolling functionality for tabs

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,43 @@
 jQuery(document).ready(function($) {
-  $(window).scroll(function() {
-    var scrollPos = $(window).scrollTop(),
-        navbar = $('.navbar-default');
+  let $tabsContainer = $(".tabs"),
+      $tabIndicator = $("#tab-indicator");
 
-    if (scrollPos > 20) {
-      navbar.addClass('change-color');
-    } else {
-      navbar.removeClass('change-color');
-    }
-  });
+
+['bio', 'experience', 'reviews', 'resources'].forEach(tab => {
+  let $newTab = $(`.tab-link.${tab}`);
+
+  $newTab.click(function(e){
+    let $currentTab = $("[aria-selected='true']"),
+        currentTabPosition = $currentTab.offset().left;
+
+    // Turn current tab off
+    $currentTab.attr("aria-selected", "false")
+
+    // Set the clicked one on
+    $newTab.attr("aria-selected", "true")
+    moveIndicator($newTab, currentTabPosition, tab);
+  })
+})
+
+
+
+
+  function moveIndicator(newTab, currentTabPosition, tab) {
+
+    let textPosition = newTab.offset().left,
+        tabsPosition = $tabsContainer.offset().left,
+        distance = textPosition - tabsPosition,
+        direction = currentTabPosition < textPosition ? 'right' : 'left';
+
+    //hide content that's currently being shown
+    $('.show-content').removeClass('show-content').css('animation', `${direction}-hide 0.3s ease-in-out`);
+
+    //show content that matches clicked tab
+    $(`.content.${tab}`).addClass('show-content').css('animation', `${direction}-show 0.3s ease-in-out`);
+
+    $tabIndicator.css('transform', `translateX(${distance}px) scaleX(${newTab.width() * 0.01})`);
+  }
+
+
 });
+
