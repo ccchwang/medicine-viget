3c90870cdf8cd8267565a4fe61c747e700b15500
Chloe Hwang <c.chloeh@gmail.com>
2017-06-15 19:50:02 -0400

    got texting animation to work

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,19 @@
-$(document).ready(function () {
+window.onload = function() {
+  let chatList = document.getElementsByClassName('bubble')
 
-  let $tabsContainer = $(".tabs"),
-      $tabIndicator = $("#tab-indicator");
+  Array.prototype.slice.call(chatList).forEach(text => {
 
-  //add click handlers to each tab
-  ['bio', 'experience', 'reviews', 'resources'].forEach(tab => {
-    let $newTab = $(`.tab-link.${tab}`);
+    text.style.animationDelay = `${text.dataset.delay}ms`;
+    text.style.animationPlayState = 'running';
+    text.className += " finished";
 
-    $newTab.click(() => {
-      let $currentTab = $("[aria-selected='true']"),
-          currentTabPosition = $currentTab.offset().left;
 
-      //turn current tab off
-      $currentTab.attr("aria-selected", "false")
+    if (!text.className.includes('-response')) {
+      Array.prototype.slice.call(text.children).forEach(child => {
+        child.style.transitionDelay = `${1000 + Number(text.dataset.delay)}ms`;
+      })
+    }
 
-      //turn the clicked one on
-      $newTab.attr("aria-selected", "true")
-
-      //animate transition from one tab to another
-      handleTransitionAnim($newTab, currentTabPosition, tab);
-    })
   })
-
-
-  function handleTransitionAnim($newTab, currentTabPosition, tab) {
-    let textPosition = $newTab.offset().left,
-        tabsPosition = $tabsContainer.offset().left,
-        distance = textPosition - tabsPosition,
-        //direction that tab is moving towards will influence which animation gets triggered
-        direction = currentTabPosition < textPosition ? 'right' : 'left';
-
-    //hide content that's currently being shown
-    $('.show-content').removeClass('show-content').css('animation', `${direction}-hide 0.3s ease-in-out`);
-
-    //show content that matches clicked tab
-    $(`.content.${tab}`).addClass('show-content').css('animation', `${direction}-show 0.3s ease-in-out`);
-
-    //move indicator and scale it to fit tab
-    $tabIndicator.css('transform', `translateX(${distance}px) scaleX(${($newTab.width() + 19) * 0.01})`);
-  }
-});
+}
 
